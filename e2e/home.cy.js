9c726fa90243878a33347636ae709946f73dd8f1
MOUKOURI Victor Eric <victorjoluke190701@gmail.com>
2025-08-17 23:43:42 +0100

    Push

---
+++ e2e/home.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe("Vérification du titre de la page", () => {
+  beforeEach(() => {
+    // Charge la page (grâce au baseUrl défini dans cypress.config.js)
+    cy.visit("http://127.0.0.1:3000/adwa/index.html");
+  });
+
+  it("Le titre doit être 'Formulaire Landing Page'", () => {
+    cy.title().should("eq", "Formulaire Landing Page");
+  });
+});
+
+describe("Formulaire -> Enregistrement dans data.json", () => {
+  beforeEach(() => {
+    // Vide le fichier data.json avant chaque test
+    // cy.request("POST", "http://127.0.0.1:3000/adwa/index.html/reset");
+    // cy.visit("http://127.0.0.1:3000/adwa/index.html");
+  });
+});
